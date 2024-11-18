03b309f60a4deba70e0f66ea01478af81a95a0c0
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-11-17 22:38:24 -0300

    fix: Correção do IUserAdditionalDataRepository

--- APITechZap/Repository/Interface/IUserAdditionalDataRepository.cs
+++ APITechZap/Repository/Interface/IUserAdditionalDataRepository.cs
@@ -4,9 +4,7 @@ namespace APITechZap.Repository.Interface;
 
 public interface IUserAdditionalDataRepository
 {
-    Task<IEnumerable<UserAdditionalData>> GetAllUserAdditionalDatasAsync();
-    Task<UserAdditionalData?> GetUserAdditionalDataByIdAsync(int id);
     Task <string> AddUserAdditionalDataAsync(UserAdditionalData userAdditionalData);
     Task <string> UpdateUserAdditionalDataAsync(UserAdditionalData userAdditionalData);
-    Task <string> DeleteUserAdditionalDataAsync(int id);
+    Task<UserAdditionalData> GetUserAdditionalDataByUserIdAsync(int userId);
 }
