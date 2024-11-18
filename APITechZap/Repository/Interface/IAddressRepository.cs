dce42baee1f40806755fbc65d21fb4f9821d844b
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-11-17 22:38:03 -0300

    fix: Corrigido o IAddressRepository

--- APITechZap/Repository/Interface/IAddressRepository.cs
+++ APITechZap/Repository/Interface/IAddressRepository.cs
@@ -4,9 +4,7 @@ namespace APITechZap.Repository.Interface;
 
 public interface IAddressRepository
 {
-    Task<IEnumerable<Address>> GetAllAddressesAsync();
-    Task<Address?> GetAddressByIdAsync(int id);
     Task <string> AddAddressAsync(Address address);
     Task <string> UpdateAddressAsync(Address address);
-    //Task <string> DeleteAddressAsync(int id);
+    Task<Address> GetAddressByUserIdAsync(int id);
 }
