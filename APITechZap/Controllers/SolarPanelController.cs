da804b53732aa685bb6e5a5d6a5226a3e4c53e56
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-11-19 22:55:52 -0300

    feat: Adicionados os Controllers SolarPanel e WindTurbine

---
+++ APITechZap/Controllers/SolarPanelController.cs
@@ -0,0 +1,65 @@
+﻿using APITechZap.Repository.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APITechZap.Controllers;
+
+/// <summary>
+/// Wind Turbine Controller
+/// </summary>
+[Route("api/solar-panel")]
+[ApiController]
+public class SolarPanelController : ControllerBase
+{
+
+    private readonly ISolarPanelRepository _solarPanelRepository;
+
+    /// <summary>
+    /// Constructor for SolarPanelController
+    /// </summary>
+    /// <param name="solarPanelRepository"></param>
+    public SolarPanelController(ISolarPanelRepository solarPanelRepository)
+    {
+        _solarPanelRepository = solarPanelRepository;
+    }
+
+    /// <summary>
+    /// Get all Solar Panels
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> GetAllSolarPanelsAsync()
+    {
+        try
+        {
+            var solarPanels = await _solarPanelRepository.GetAllSolarPanelsAsync();
+
+            return Ok(solarPanels);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    /// <summary>
+    /// Get Solar Panel by Id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetSolarPanelByIdAsync(int id)
+    {
+        try
+        {
+            var solarPanel = await _solarPanelRepository.GetSolarPanelByIdAsync(id);
+
+            return Ok(solarPanel);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+}
---
+++ APITechZap/Controllers/WindTurbineController.cs
@@ -0,0 +1,63 @@
+﻿using APITechZap.Repository.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APITechZap.Controllers;
+
+/// <summary>
+/// Wind Turbine Controller
+/// </summary>
+[Route("api/wind-turbine")]
+[ApiController]
+public class WindTurbineController : ControllerBase
+{
+    private readonly IWindTurbineRepository _windTurbineRepository;
+
+    /// <summary>
+    /// Constructor for WindTurbineController
+    /// </summary>
+    /// <param name="windTurbineRepository"></param>
+    public WindTurbineController(IWindTurbineRepository windTurbineRepository)
+    {
+        _windTurbineRepository = windTurbineRepository;
+    }
+
+    /// <summary>
+    /// Get all Wind Turbines
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> GetAllWindTurbinesAsync()
+    {
+        try
+        {
+            var windTurbines = await _windTurbineRepository.GetAllWindTurbinesAsync();
+
+            return Ok(windTurbines);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    /// <summary>
+    /// Get Wind Turbine by Id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetWindTurbineByIdAsync(int id)
+    {
+        try
+        {
+            var windTurbine = await _windTurbineRepository.GetWindTurbineByIdAsync(id);
+
+            return Ok(windTurbine);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+}
